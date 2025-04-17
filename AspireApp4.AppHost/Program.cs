8747672cf93987552f8c8cddcd52a7cc7805780d
Rick Neeft <rick.neeft@vecozo.nl>
2025-04-17 21:58:16 +0200

    Update issue with an extra set of Console apps - Working

--- AspireApp4.AppHost/Program.cs
+++ AspireApp4.AppHost/Program.cs
@@ -1,4 +1,40 @@
+using Projects;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+// Fake a SQL Server
+var app1 = builder
+    .AddProject<ConsoleApp1>("sql-app")
+    .WithArgs("-1");
+
+var db1App = builder
+    .AddProject<Projects.ConsoleApp1>("db1-app")
+    .WaitFor(app1)
+    .WithParentRelationship(app1)
+    .WithArgs("-1");
+
+var db2App = builder
+    .AddProject<Projects.ConsoleApp1>("db2-app")
+    .WaitFor(app1)
+    .WithParentRelationship(app1)
+    .WithArgs("-1");
+
+var migration1App = builder
+    .AddProject<Projects.ConsoleApp1>("db1-migrations-app")
+    .WithArgs("2000")
+    .WithReference(db1App)
+    .WaitFor(db1App)
+    .WithParentRelationship(db1App);
+
+var migration2App = builder
+    .AddProject<Projects.ConsoleApp1>("db2-migrations-app")
+    .WithArgs("2000")
+    .WithReference(db2App)
+    .WaitFor(db2App)
+    .WithParentRelationship(db2App)
+    .WithExplicitStart();
+
+// Real SQL server
 
 var sqlServer = builder
     .AddSqlServer("sql", port: 1234)
@@ -8,6 +44,7 @@ var sqlServer = builder
 var db1 = sqlServer.AddDatabase("db1");
 var migration1 = builder
     .AddProject<Projects.ConsoleApp1>("db1-migrations")
+    .WithArgs("2000")
     .WithReference(db1)
     .WaitFor(db1)
     .WithParentRelationship(db1);
@@ -15,9 +52,11 @@ var migration1 = builder
 var db2 = sqlServer.AddDatabase("db2");
 var migration2 = builder
     .AddProject<Projects.ConsoleApp2>("db2-migrations")
+    .WithArgs("2000")
     .WithReference(db2)
     .WaitFor(db2)
     .WithParentRelationship(db2)
     .WithExplicitStart();
+
 
 builder.Build().Run();
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+try
+{
+    var wait = int.Parse(args[0]);
+    Thread.Sleep(wait);
+}
+finally { }
